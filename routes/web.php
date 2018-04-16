bdf9d2ba7d471db0cd2961575747e51ea1ee61c7
Manu <realgarcilaso@outlook.com>
2018-04-16 07:53:08 -0500

    07 mas ajustes acerca de middleware
    le agrego mas ajustes, es decir que esta protegiendo algunas paginas
    donde solo el admin debe ingresar. tambien cada vez que visitemos una
    URL de web.php se haran atraves del middleware de las cookies encriptadas.

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+
 
 Route::group(['prefix' => 'admin', 'middleware'=> 'auth'], function (){
 
+    Route::get('/home', [
+
+        'uses' => 'HomeController@index',
+        'as' => 'home'
+    ]);
 
     Route::get('/post/create', [
 
